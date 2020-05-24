e27ae4d50989d0651c54539a80240f70f1a92a13
Taragos <ole.gensch@pm.me>
2020-05-24 08:21:33 +0200

    start stop service

--- src/coordinator.py
+++ src/coordinator.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 import rospy 
 from geometry_msgs.msg import Twist
+from std_srvs.srv import SetBool, SetBoolResponse
+
+startStop = False
 
 def coordinator():
-  pub = rospy.Publisher('cmd_vel',Twist,queue_size=10 )
   rospy.init_node('coordinator', anonymous=True)
+
+  pub = rospy.Publisher('cmd_vel',Twist,queue_size=10 )
+  rospy.Service('start_stop', SetBool, start_stop)
+
   rate = rospy.Rate(10)
 
   move_forward = Twist()
@@ -14,6 +20,16 @@ def coordinator():
     rospy.loginfo("Moving forward, biatch")
     pub.publish(move_forward)
     rate.sleep()
+
+
+def start_stop(msg):
+    global startStop
+    if msg.data:
+        rospy.loginfo("Stopping")
+    else:
+        rospy.loginfo("Starting")
+    startStop = msg.data
+    return SetBoolResponse(startStop, "")
 
 if __name__ == '__main__':
   try:
