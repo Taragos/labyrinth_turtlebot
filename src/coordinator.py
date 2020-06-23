5669d8504d7c93e5223a2aa314d01c2e3a807b97
Razurio <private.ente@gmail.com>
2020-06-23 13:19:17 +0200

    Entry Markers  und A* Marker Funktion hinzugefügt

--- src/coordinator.py
+++ src/coordinator.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
+import math
+
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Point, PoseWithCovariance
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Bool
-from std_srvs.srv import SetBool
+from nav_msgs.msg import Odometry
+from rospy import Time
+from std_srvs.srv import SetBoolResponse, SetBool
+from tf.transformations import euler_from_quaternion
+from visualization_msgs.msg import Marker
 
 srv_client_find_the_entry_ = None
 srv_client_wall_follower_ = None
@@ -11,6 +17,12 @@ startStop_ = False
 regions_ = None
 state_desc_ = ['Find the entry', 'wall following', 'Go to point']
 state_ = 0
+pub_visualization_marker_ = None
+orientation_ = Point()
+position_ = Point()
+roll = pitch = yaw = 0.0
+x = y = 0.0
+yaw_degree = 0.0
 
 
 # callbacks
@@ -44,8 +56,73 @@ def change_state(state):
         resp = srv_client_wall_follower_(True)
 
 
+def position(msg):
+    global orientation_, position_, roll, pitch, yaw, yaw_degree  # position_marker,
+    position_ = msg.pose.pose.position
+    orientation_ = msg.pose.pose.orientation
+    (roll, pitch, yaw) = euler_from_quaternion([orientation_.x, orientation_.y, orientation_.z, orientation_.w])
+    yaw_degree = (yaw * (180 / math.pi))
+
+    print('X: ' + str(position_.x) + ' / Y: ' + str(position_.y))
+    print('Yaw: ' + str(yaw) + '\n')
+    print('Yaw Degree: ' + str(yaw_degree))
+    print('Marker Position: ' + str(x) + '/' + str(y))
+
+
+def marker(x, y):
+    global pub_visualization_marker_
+
+    marker = Marker()
+    marker.header.frame_id = "odom"
+    marker.header.stamp = Time()
+    marker.ns = "labyrinth_turtlebot_algo"
+    marker.id = 0
+    marker.text = "Algo_Marker"
+    marker.type = Marker.CYLINDER
+    marker.action = Marker.ADD
+    marker.pose.position.x = x
+    marker.pose.position.y = y
+    marker.pose.position.z = 1
+    marker.pose.orientation.x = 0.0
+    marker.pose.orientation.y = 0.0
+    marker.pose.orientation.z = 0.0
+    marker.pose.orientation.w = 1.0
+    marker.scale.x = 0.2
+    marker.scale.y = 0.2
+    marker.scale.z = 2
+    marker.color.a = 1.0
+    marker.color.r = 0.0
+    marker.color.g = 1.0
+    marker.color.b = 1.0
+
+    pub_visualization_marker_.publish(marker)
+
+
+def handle_marker_req(req):
+    global x, y
+    if req.data:
+        if 0 <= yaw_degree <= 90:
+            x = position_.x + (10 * math.cos(yaw_degree))
+            y = position_.y + (10 * math.sin(yaw_degree))
+        elif 90 < yaw_degree <= 180:
+            x = position_.x - (10 * math.cos(180 - yaw_degree))
+            y = position_.y + (10 * math.sin(180 - yaw_degree))
+        elif 180 < yaw_degree < 270:
+            -x = position_.x - (10 * math.cos(180 - abs(yaw_degree)))
+            -y = position_.y + (10 * math.sin(180 - abs(yaw_degree)))
+        else:
+            -x = position_.x + (10 * math.cos(abs(yaw_degree)))
+            -y = position_.y + (10 * math.sin(abs(yaw_degree)))
+
+        marker(x, y)
+        rospy.loginfo("Set new Marker")
+        return SetBoolResponse(success=True, message="New Marker was set")
+    else:
+        return SetBoolResponse(success=False)
+
+
 def coordinator():
-    global srv_client_find_the_entry_, srv_client_wall_follower_
+    global srv_client_find_the_entry_, srv_client_wall_follower_, pub_visualization_marker_
     global regions_, state_, startStop_
 
     rospy.init_node('coordinator')
@@ -61,6 +138,10 @@ def coordinator():
     sub_drive = rospy.Subscriber('/start_stop', Bool, clbk_drive)
     srv_client_find_the_entry_ = rospy.ServiceProxy('/find_the_entry_switch', SetBool)
     srv_client_wall_follower_ = rospy.ServiceProxy('/wall_follower_switch', SetBool)
+
+    rospy.Service('set_marker', SetBool, handle_marker_req)
+    sub_odom = rospy.Subscriber('/odom', Odometry, position)
+    pub_visualization_marker_ = rospy.Publisher("/visualization_marker", Marker, queue_size=1)
 
     rate = rospy.Rate(20)
 
--- src/find_the_entry.py
+++ src/find_the_entry.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+import math
 
 import rospy
 from nav_msgs.msg import Odometry
@@ -7,12 +8,13 @@ from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist, Point
 from std_msgs.msg import Bool
 from std_srvs.srv import *
-from visualization_msgs.msg import Marker
+from visualization_msgs.msg import Marker, MarkerArray
 
 position_ = Point()
 active_ = False
 pub_cmd_vel_ = None
 pub_visualization_marker_ = None
+pub_visualization_marker_entries_ = None
 regions_ = {
     'right': 0,
     'fright': 0,
@@ -22,6 +24,9 @@ regions_ = {
 }
 entry_found_ = 0
 startStop_ = False
+dist_left = 0.0
+dist_right = 0.0
+marker_entries_ = MarkerArray()
 
 
 def find_the_entry_switch(req):
@@ -37,12 +42,11 @@ def find_the_entry_switch(req):
 def clbk_odom(msg):
     global position_
 
-    # position
     position_ = msg.pose.pose.position
 
 
 def clbk_laser(msg):
-    global regions_
+    global regions_, dist_left, dist_right
     regions_ = {
         'right': min(min(msg.ranges[72:107]), 10),
         'fright': min(min(msg.ranges[36:71]), 10),
@@ -50,6 +54,9 @@ def clbk_laser(msg):
         'fleft': min(min(msg.ranges[288:323]), 10),
         'left': min(min(msg.ranges[252:287]), 10),
     }
+
+    dist_right = min(min(msg.ranges[16:90]), 3.6)
+    dist_left = min(min(msg.ranges[270:345]), 3.6)
 
     take_action()
 
@@ -67,6 +74,56 @@ def take_action():
         if regions['left'] < 1.2 or regions['right'] < 1.2:
             entry_found_ = 1
             marker(int(position_.x.real), int(position_.y.real))
+
+            pos_y = int(position_.y.real)
+
+            if 0.49 < dist_left < 1 or 1.49 < dist_left < 2 or 2.49 < dist_left < 3:
+                pos_y_left = pos_y + int(math.ceil(dist_left))
+            else:
+                pos_y_left = pos_y + int(math.floor(dist_left))
+
+            if 0.5 < dist_right < 1 or 1.5 < dist_right < 2 or 2.5 < dist_right < 3:
+                pos_y_right = pos_y - int(math.ceil(dist_right))
+            else:
+                pos_y_right = pos_y - int(math.floor(dist_right))
+
+            entry_markers(int(position_.x.real), pos_y_left)
+            entry_markers(int(position_.x.real), pos_y_right)
+
+
+def entry_markers(x, y):
+    global pub_visualization_marker_entries_
+
+    marker = Marker()
+    marker.header.frame_id = "odom"
+    marker.header.stamp = Time()
+    marker.ns = "labyrinth_entries"
+    marker.text = "Entry Wall"
+    marker.type = Marker.CYLINDER
+    marker.action = Marker.ADD
+    marker.pose.position.x = x
+    marker.pose.position.y = y
+    marker.pose.position.z = 1
+    marker.pose.orientation.x = 0.0
+    marker.pose.orientation.y = 0.0
+    marker.pose.orientation.z = 0.0
+    marker.pose.orientation.w = 1.0
+    marker.scale.x = 0.2
+    marker.scale.y = 0.2
+    marker.scale.z = 2
+    marker.color.a = 1.0
+    marker.color.r = 0.0
+    marker.color.g = 1.0
+    marker.color.b = 0.0
+
+    marker_entries_.markers.append(marker)
+
+    id = 0
+    for marker in marker_entries_.markers:
+        marker.id = id
+        id += 1
+
+    pub_visualization_marker_entries_.publish(marker_entries_)
 
 
 def find_entry():
@@ -97,20 +154,22 @@ def marker(x, y):
     marker.scale.y = 0.5
     marker.scale.z = 2
     marker.color.a = 1.0
-    marker.color.r = 0.0
-    marker.color.g = 1.0
+    marker.color.r = 1.0
+    marker.color.g = 0.0
     marker.color.b = 0.0
+    rospy.loginfo(marker)
     pub_visualization_marker_.publish(marker)
 
 
 def main():
-    global pub_cmd_vel_, pub_visualization_marker_, active_
+    global pub_cmd_vel_, pub_visualization_marker_, active_, pub_visualization_marker_entries_
 
     rospy.init_node('find_the_entry')
 
     pub_cmd_vel_ = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 
     pub_visualization_marker_ = rospy.Publisher("/visualization_marker", Marker, queue_size=1)
+    pub_visualization_marker_entries_ = rospy.Publisher("/visualization_marker_array", MarkerArray, queue_size=2)
 
     sub_scan = rospy.Subscriber('/scan', LaserScan, clbk_laser)
 
@@ -120,7 +179,7 @@ def main():
 
     srv = rospy.Service('find_the_entry_switch', SetBool, find_the_entry_switch)
 
-    rate = rospy.Rate(20)
+    rate = rospy.Rate(1)
     while not rospy.is_shutdown():
 
         msg = Twist()
