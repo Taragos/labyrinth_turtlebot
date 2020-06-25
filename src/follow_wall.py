d885da07bdd0c3e842d52a49ef2aea03587d0f8a
Felix Ricke <rickefelix@web.de>
2020-06-25 18:47:36 +0200

    playing around

--- src/follow_wall.py
+++ src/follow_wall.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python
+import math
 
 import rospy
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Point
 from std_msgs.msg import Bool
 from std_srvs.srv import *
+from tf.transformations import euler_from_quaternion
 
 active_ = False
 pub_cmd_vel_ = None
@@ -19,17 +21,33 @@ entry_found_ = 0
 state_ = 0
 state_dict_ = {
     0: 'find the wall',
-    1: 'turn left',
-    2: 'turn right',
+    1: 'turn right',
+    2: 'turn left',
     3: 'follow the wall',
     4: 'turn right on point',
     5: 'turn left on point',
+    6: 'drive backwards',
 }
 startStop_ = False
 
 wall_found = 0
-wall_dist = 0.2
+wall_dist = 0.25
 max_speed = 0.05
+
+inf = 3.6
+
+orientation_ = Point()
+position_ = Point()
+roll = pitch = yaw = 0.0
+x = y = 0.0
+yaw_degree = 0.0
+desired_yaw = 0
+yaw_dict_ = {
+    0: 0,
+    1: 1.5,
+    2: 3,
+    3: -1.5,
+}
 
 
 def wall_follower_switch(req):
@@ -43,16 +61,29 @@ def wall_follower_switch(req):
 
 # callbacks
 def clbk_laser(msg):
-    global regions_
+    global regions_, inf
     regions_ = {
-        'left': min(min(msg.ranges[72:107]), 10),
-        'fleft': min(min(msg.ranges[36:71]), 10),
-        'front': min(min(min(msg.ranges[0:35]), min(msg.ranges[324:359])), 10),
-        'fright': min(min(msg.ranges[288:323]), 10),
-        'right': min(min(msg.ranges[252:287]), 10),
+        'left': min(min(msg.ranges[72:107]), inf),
+        'fleft': min(min(msg.ranges[36:71]), inf),
+        'front': min(min(min(msg.ranges[0:35]), min(msg.ranges[324:359])), inf),
+        'fright': min(min(msg.ranges[288:323]), inf),
+        'right': min(min(msg.ranges[252:287]), inf),
     }
 
     determine_direction_of_obstacle()
+
+
+def clbk_position(msg):
+    """
+    Callback for the /odom Topic
+    Get's the odometry data of the roboter and extracts valuable information
+    position_: Current world position of roboter
+    """
+    global orientation_, position_, roll, pitch, yaw, yaw_degree  # position_marker,
+    position_ = msg.pose.pose.position
+    orientation_ = msg.pose.pose.orientation
+    (roll, pitch, yaw) = euler_from_quaternion([orientation_.x, orientation_.y, orientation_.z, orientation_.w])
+    yaw_degree = (yaw * (180 / math.pi))
 
 
 def clbk_drive(msg):
@@ -76,57 +107,89 @@ def determine_direction_of_obstacle():
     regions = regions_
     state_description = ''
 
+    # rospy.loginfo("---------------------------")
+    # rospy.loginfo("Deadend: " + str(is_deadend()))
+    # rospy.loginfo("Outer Corner: " + str(is_outer_corner()))
+    # rospy.loginfo("Inner Corner: " + str(is_inner_corner()))
+    # rospy.loginfo("---------------------------")
+    #
+    # rospy.loginfo("---------------------------")
+    # rospy.loginfo("Left: " + str(wall_left()))
+    # rospy.loginfo("FLeft: " + str(wall_fleft()))
+    # rospy.loginfo("Front: " + str(wall_front()))
+    # rospy.loginfo("FRight: " + str(wall_fright()))
+    # rospy.loginfo("Right: " + str(wall_right()))
+    # rospy.loginfo("---------------------------")
+
+    rospy.loginfo("---------------------------")
+    rospy.loginfo("Left: " + str(regions_['left']))
+    rospy.loginfo("Left: " + str(regions_['fleft']))
+    rospy.loginfo("Front: " + str(regions_['front']))
+    rospy.loginfo("Right: " + str(regions_['fright']))
+    rospy.loginfo("Right: " + str(regions_['right']))
+    rospy.loginfo("---------------------------")
+
     if wall_found == 0:
         change_state(0)
     else:
-        # 1 = right, 2 = left, 3 = wall follow, 4 = point rotate
-        if regions['fright'] > wall_dist:
-            change_state(1)
-        elif regions['fright'] < wall_dist and regions['front'] < wall_dist:
+        # 0: 'find the wall', 1: 'turn left', 2: 'turn right', 3: 'follow the wall', 4: 'turn right on point',
+        # 5: 'turn left on point',
+
+        if regions_['left'] < 0.12 or regions_['fleft'] < 0.12 or regions_['front'] < 0.12 or regions_['fright'] < 0.12 or regions_['right'] < 0.12:
+            change_state(6)
+        elif is_inner_corner():
             change_state(5)
-        elif regions['front'] < wall_dist:
+        elif wall_front() and wall_fleft() and not wall_fright():
             change_state(4)
-        elif regions['fright'] < wall_dist:
+        elif wall_fright():
+            change_state(1)
+        elif not wall_fright() and not wall_right():
             change_state(2)
-        elif regions['fright'] > wall_dist > regions['front']:
-            change_state(4)
+        elif wall_front():
+            change_state(5)
         else:
-            change_state(3)
-    #
-    #     if regions['front'] > wall_dist and regions['fright'] > wall_dist:
-    #         state_description = 'case 1 - nothing'
-    #         change_state(1)
-    #     elif regions['front'] < wall_dist < regions['fright']:
-    #         state_description = 'case 2 - front'
-    #         change_state(2)
-    #     elif regions['front'] > wall_dist > regions['fright']:
-    #         state_description = 'case 3 - fright'
-    #         change_state(3)
-    #     elif regions['front'] > wall_dist and regions['fright'] > wall_dist:
-    #         state_description = 'case 4 - fleft'
-    #         change_state(1)
-    #     elif regions['front'] < wall_dist and regions['fright'] < wall_dist:
-    #         state_description = 'case 5 - front and fright'
-    #         change_state(2)
-    #     elif regions['front'] < wall_dist and regions['fleft'] < wall_dist:
-    #         state_description = 'case 6 - front and fleft'
-    #         change_state(2)
-    #     elif regions['front'] < wall_dist and regions['fright'] < wall_dist:
-    #         state_description = 'case 7 - front and fleft and fright'
-    #         change_state(2)
-    #     elif regions['front'] > wall_dist > regions['fright']:
-    #         state_description = 'case 8 - fleft and fright'
-    #         change_state(2)
-    #     else:
-    #         state_description = 'unknown case'
-    #         rospy.loginfo(regions)
-    #
-    # rospy.loginfo(state_description)
+            rospy.loginfo("Unknown state")
+
+
+def wall_left():
+    return regions_['left'] < wall_dist
+
+
+def wall_fleft():
+    return regions_['fleft'] < wall_dist
+
+
+def wall_front():
+    return regions_['front'] < wall_dist
+
+
+def wall_fright():
+    return regions_['fright'] < wall_dist
+
+
+def wall_right():
+    return regions_['right'] < wall_dist
+
+
+def is_deadend():
+    return wall_fleft() and wall_fright() and wall_front()
+
+
+def is_inner_corner():
+    return wall_fright() and wall_front() #  and not wall_left()
+
+
+def is_outer_corner():
+    return wall_right() and not wall_fright()
+
+
+def is_wall_front():
+    return (wall_fleft() or wall_front() or wall_fright()) and not wall_left() and not wall_right()
 
 
 def find_wall():
     """
-    Generates a Twist Message, that let's the robot turn left
+    Generates a Twist Message, that let's the robot turn right
     """
     global regions_, max_speed, wall_found
     regions = regions_
@@ -147,6 +210,16 @@ def turn_left():
     """
     msg = Twist()
     msg.linear.x = max_speed
+    msg.angular.z = 0.1
+    return msg
+
+
+def turn_left_on_point():
+    """
+    Generates a Twist Message, that let's the robot turn left on spot
+    """
+    msg = Twist()
+    msg.linear.x = 0
     msg.angular.z = 0.5
     return msg
 
@@ -157,23 +230,13 @@ def turn_right():
     """
     msg = Twist()
     msg.linear.x = max_speed
-    msg.angular.z = -0.5
+    msg.angular.z = -0.1
     return msg
 
 
 def turn_right_on_point():
     """
-    Generates a Twist Message, that let's the robot turn right
-    """
-    msg = Twist()
-    msg.linear.x = 0
-    msg.angular.z = -0.5
-    return msg
-
-
-def turn_left_on_point():
-    """
-    Generates a Twist Message, that let's the robot turn right
+    Generates a Twist Message, that let's the robot turn right on spot
     """
     msg = Twist()
     msg.linear.x = 0
@@ -190,6 +253,18 @@ def follow_the_wall():
     msg = Twist()
     msg.linear.x = max_speed
     msg.angular.z = 0
+    return msg
+
+
+def drive_backwards():
+    """
+    Generates a Twist Message, that let's the robot drive backwards
+    """
+    global max_speed
+
+    msg = Twist()
+    msg.linear.x = -max_speed
+    msg.angular.z = -0.3
     return msg
 
 
@@ -224,9 +299,11 @@ def main():
         elif state_ == 3:
             msg = follow_the_wall()
         elif state_ == 4:
-            msg = turn_right()
+            msg = turn_right_on_point()
         elif state_ == 5:
             msg = turn_left_on_point()
+        elif state_ == 6:
+            msg = drive_backwards()
             pass
         else:
             rospy.logerr('Unknown state!')
